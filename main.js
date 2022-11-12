9bc54719cc2f63f89b7b51e4a41d59de6a6837d8
Elcin Quliyev <wdplay75@gmail.com>
2022-11-12 17:39:58 +0400

    tedt

--- main.js
+++ main.js
@@ -229,3 +229,62 @@
 // } else {
 //   console.log("Tranparen alinmadi");
 // }
+//.14 Test No:14 kodda bezi deisiklik olmali..?
+// let valida = prompt("1 ci teref");
+// let validb = prompt("2 ci teref");
+// let validc = prompt("3 cu teref");
+
+// if (valida + validb > validc) {
+//   console.log("3 cu teref kicik");
+// } else if (valida + validc > validb) {
+//   console.log("2 ci teref kicik");
+// } else if (validb + validc > valida) {
+//   console.log("1 ci teref kicik");
+// } else {
+//   console.log("Duzgun qiymetlendir");
+// }
+//.15 test No:15
+// let valida = prompt("1 ci teref");
+// let validb = prompt("2 ci teref");
+// let validc = prompt("3 cu teref");
+
+// if (valida == validb && validb == validc) {
+//   console.log("Beraber terefli");
+// } else if (valida == validb || validb == validc || valida == validc) {
+//   console.log("iti bucaq");
+// } else {
+//   console.log("Yeniden qiymetlendir.");
+// }
+//.16 Test No:16 pow quvvete yukseltme sqrt kokden cixartma
+// let xElement = prompt();
+// let yElement = prompt();
+// let cElement = prompt();
+// let discriminant = yElement * yElement - 4 * xElement * cElement;
+// console.log(discriminant);
+//.17 Test No:17
+// let costPrice = prompt("Xerc mebleginiz ");
+// let sellPrice = prompt("Satis mebleginiz");
+// let profit2 = costPrice - sellPrice; //zerer
+// let profit1 = sellPrice - costPrice; //menfeet
+
+// if (profit1 > profit2) {
+//   console.log("Yaxsi Qazanc + profit1");
+// } else if (profit2 > profit1) {
+//   console.log("Pis Qazanc + profit2");
+// } else {
+//   console.log("Qazanc Yoxdur");
+// }
+//.18 Test No:18 kodlar eror..!
+// let inputmark = prompt();
+// let input2 = prompt();
+// let input3 = prompt();
+// let input4 = prompt();
+// let input5 = prompt();
+// let calculate = (inputmark + input2 + input3 + input4 + input5) / 5.0;
+
+// if (calculate >= 90) {
+//   consol.log(calculat + "A");
+// } else if (calculate >= 80) {
+//   console.log("B");
+// }
+//.19 Test No19
